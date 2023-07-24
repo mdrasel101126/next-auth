1250ce252060d3ac0e8ed068b800bc5953c67c9a
mdrasel101126 <mdrasel101126@gmail.com>
2023-07-25 00:31:14 +0600

    Google and Github authentication added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import Head from "next/head";
+import { useSession } from "next-auth/react";
 const HomePage = () => {
+  const { data: session } = useSession();
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop:"10%" }}>Welcome To Next Auth Home Page</h1>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+        Welcome To Next Auth Home Page
+      </h1>
+      <h3 style={{ textAlign: "center", marginTop: "10%" }}>
+        Wellcome Mr. {session?.user?.name}
+      </h3>
     </div>
   );
 };
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,7 @@ import { Button } from "antd";
 import { GoogleOutlined, GithubOutlined } from "@ant-design/icons";
 import Head from "next/head";
 import styles from "@/styles/Login.module.css";
+import { signIn } from "next-auth/react";
 const LoginPage = () => {
   return (
     <div>
@@ -11,8 +12,16 @@ const LoginPage = () => {
       <div className={styles.form}>
         <h3>LOGIN</h3>
         <div className={styles.social_icons}>
-          <GoogleOutlined />
-          <GithubOutlined />
+          <GoogleOutlined
+            onClick={() =>
+              signIn("google", { callbackUrl: "http://localhost:3000" })
+            }
+          />
+          <GithubOutlined
+            onClick={() =>
+              signIn("github", { callbackUrl: "http://localhost:3000" })
+            }
+          />
         </div>
         <hr />
         <form>

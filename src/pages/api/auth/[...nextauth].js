9c0852fc985b0322a28cfa2aea71b8b1c414840d
mdrasel101126 <mdrasel101126@gmail.com>
2023-07-25 22:59:39 +0600

    private route system added

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -14,5 +14,8 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  pages: {
+    signIn: "/login",
+  },
 };
 export default NextAuth(authOptions);
